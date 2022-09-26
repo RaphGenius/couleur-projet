e05e5199fb725132c68136ab2a9557f3b4ff1f17
Raphael <guet.raphael@hotmail.com>
2022-09-26 16:18:02 +0200

    Appli fini

--- script.js
+++ script.js
@@ -33,5 +33,64 @@ function adaptInputColor() {
   colorLabels.forEach((label) => {
     const hexColor = label.textContent.replace("#", "");
     const red = parseInt(hexColor.slice(0, 2), 16);
+    const green = parseInt(hexColor.slice(2, 4), 16);
+    const blue = parseInt(hexColor.slice(4, 6), 16);
+    const yik = (red * 299 + green * 587 + blue * 114) / 1000;
+
+    if (yik >= 128) {
+      label.style.color = "#111";
+    } else {
+      label.style.color = "#f1f1f1";
+    }
   });
+}
+
+const rangerInput = document.querySelector(".inp-rang");
+rangerInput.addEventListener("input", handleInclination);
+
+function handleInclination() {
+  gradientData.angle = rangerInput.value;
+  rangeLabelValue.textContent = `${gradientData.angle}
+`;
+  populateUI();
+}
+
+colorPickerInputs.forEach((input) => {
+  input.addEventListener("input", colorInputModification);
+});
+function colorInputModification(e) {
+  const currentIndex = colorPickerInputs.indexOf(e.target);
+
+  gradientData.colors[currentIndex] = e.target.value.toUpperCase();
+  populateUI();
+}
+
+const copyBtn = document.querySelector(".copy-btn");
+copyBtn.addEventListener("click", handleGradientCopy);
+
+let lock = false;
+function handleGradientCopy() {
+  const gradient = `linear-gradient(${gradientData.angle}deg, ${gradientData.colors[0]}, ${gradientData.colors[1]})`;
+  navigator.clipboard.writeText(gradient);
+
+  if (lock) return;
+  lock = true;
+  copyBtn.classList.add("active");
+  setTimeout(() => {
+    copyBtn.classList.remove("active");
+    lock = false;
+  }, 1000);
+}
+
+const randomGradientBtn = document.querySelector(".random-btn");
+
+randomGradientBtn.addEventListener("click", createRandomGradient);
+
+function createRandomGradient() {
+  for (let i = 0; i < colorLabels.length; i++) {
+    randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    console.log(randomColor);
+    gradientData.colors[i] = randomColor.toUpperCase();
+  }
+  populateUI();
 }
